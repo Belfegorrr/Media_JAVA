c9309d701a3c4daaa74da25d3d28c92962f6f5d8
Belfegorrr <pelichek.iago@gmail.com>
2023-11-18 14:00:59 -0300

    feat: Unboxing

---
+++ src/tarefa_casting/Casting.java
@@ -0,0 +1,14 @@
+package Media_JAVA.src.tarefa_casting;
+
+public class Casting {
+    
+    public static void main(String args[]) {
+        boolean status = true;
+        System.out.println(status);
+        Boolean status2 = Boolean.TRUE;
+        System.out.println(status2);
+        
+        boolean status3 = Boolean.FALSE;
+        System.out.println(status3);
+    }
+}
